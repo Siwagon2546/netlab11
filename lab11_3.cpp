e5044b431df44db725a136b292a5c580b45733d3
Siwagon2546 <netevgjqw@gmail.com>
2023-01-03 16:36:19 +0700

    lab11_3.cpp complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream numset;
+    numset.open("score.txt");
+    string txt;
+    float sum,sumsqrt;
+    int i =0;
+    while(getline(numset,txt)){
+        sum += atof(txt.c_str());
+        sumsqrt += pow(atof(txt.c_str()),2);
+        i++;
+
+    }
+    double mean = sum/i;
+    double Sta = sqrt((sumsqrt/i)-pow(mean,2));
+    cout << "Number of data = "<<i<<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<<mean<<"\n";
+    cout << "Standard deviation = "<<Sta<<"\n";
+}
